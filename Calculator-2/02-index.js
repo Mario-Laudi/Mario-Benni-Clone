729b7ef5de219a2ab54af70b3e6565eb7a820bce
Mario <mario.lauudi@dci-student.org>
2024-04-17 19:42:10 +0200

    fertig

--- Calculator-2/02-index.js
+++ Calculator-2/02-index.js
@@ -1,5 +1,7 @@
 const prompt = require("prompt-sync")({ sigint: true });
-
+/*Diese Zeile importiert das prompt-sync-Modul und initialisiert die prompt-Funktion 
+für die Synchronisation von Benutzereingaben über die Konsole.
+*/
 class Calculator {
   constructor() {
     this.PI = Math.PI;
@@ -25,10 +27,14 @@ class Calculator {
 
 function abfrage() {
   const calc = new Calculator();
-
+  /*Die Zeile erstellt eine neue Instanz der Klasse Calculator und weist sie der 
+  Konstanten calc zu, so dass Sie Methoden und Eigenschaften dieser Instanz über 
+  calc verwenden können.
+*/
   let answer = prompt(
     "Herzlich willkommen, was dürfen wir für Sie berechnen? (Addition? Subtraktion? Multiplikation? Division?)"
   );
+  //Die Funktion parseFloat() wird in JavaScript verwendet, um einen String in eine Fließkommazahl (Gleitkommazahl) umzuwandeln.
 
   if (answer.toLowerCase() === "addition") {
     let num1 = parseFloat(prompt("Bitte die erste Zahl eingeben: "));
